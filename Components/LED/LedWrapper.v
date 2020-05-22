b698a2fec506da9cf63dfd932610ad8e5fe55f14
Banyc <36535895+Banyc@users.noreply.github.com>
2020-05-23 00:34:28 +0800

    add wrappers

---
+++ Components/LED/LedWrapper.v
@@ -0,0 +1,29 @@
+`include "./Components/LED/LED_DRV.v"
+`include "./Components/clock/clkdiv.v"
+
+module LedWrapper(
+    clk,
+    rst,
+    bitmap,
+    LED_CLK,
+    LED_DO,
+);
+    input wire clk;
+    input wire rst;
+    input wire [15:0] bitmap;
+    output wire LED_CLK;
+    output wire LED_DO;
+
+    wire [31:0] clk_counter;
+
+    clkdiv slowed_clock(.clk(clk), .rst(rst), .clkdiv(clk_counter));
+
+    LED_DRV drv(
+        .clk(clk),
+        .start(clk_counter[18]),
+        .led_to_show(bitmap),
+        .finish(),
+        .serial_clk(LED_CLK),
+        .serial_led(LED_DO)
+    );
+endmodule // LedWrapper
---
+++ Components/hexDisplay/SegWrapper.v
@@ -0,0 +1,35 @@
+`include "./Components/hexDisplay/SEG_DRV.v"
+`include "./Components/clock/clkdiv.v"
+
+module SegWrapper(
+    clk,
+    rst,
+    ens,
+    nums,
+    points,
+    SEG_CLK,
+    SEG_DT
+);
+    input wire clk;
+    input wire rst;
+    input wire [7:0] ens;
+    input wire [31:0] nums;
+    input wire [7:0] points;
+    output wire SEG_CLK;
+    output wire SEG_DT;
+
+    wire [31:0] clk_counter;
+
+    clkdiv slowed_clock(.clk(clk), .rst(rst), .clkdiv(clk_counter));
+
+    SEG_DRV drv(
+        .clk(clk),
+        .start(clk_counter[18]),
+        .en(ens),
+        .num(nums),
+        .point(points),
+        .finish(),
+        .serial_clk(SEG_CLK),
+        .serial_seg(SEG_DT)
+    );
+endmodule // SegWrapper
