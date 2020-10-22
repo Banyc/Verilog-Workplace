5f16d2fd89ddeb7f0eb463cef1cea05f49de5bc3
Banyc <36535895+Banyc@users.noreply.github.com>
2020-10-22 22:46:20 +0800

    [clk] change variable name

--- Components/clock/clk_2to1.v
+++ Components/clock/clk_2to1.v
@@ -1,12 +1,12 @@
 `ifndef __clk_2to1__
 `define __clk_2to1__
 
-module clk_2to1(input clk200P, clk200N, rst, output clk);
-	IBUFDS sclk(
-		.I(clk200P),
-		.IB(clk200N),
-		.O(clk)
-	);
+module clk_2to1(input clk200P, clk200N, rst, output clk200MHz);
+    IBUFDS sclk(
+        .I(clk200P),
+        .IB(clk200N),
+        .O(clk200MHz)
+    );
 endmodule
 
 `endif
