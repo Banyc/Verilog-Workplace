3fa98a935514c3c30ae46b4f2eea7ec7bcb1cff5
Banyc <36535895+Banyc@users.noreply.github.com>
2020-10-22 23:18:04 +0800

    [enum] split enum from control.v

--- Components/cpu/riscv/1stage/RiscV1StageControl.v
+++ Components/cpu/riscv/1stage/RiscV1StageControl.v
@@ -7,28 +7,7 @@
 `include "Components/cpu/riscv/EnumFunct10.vh"
 `include "Components/cpu/riscv/EnumInstructionTypes.vh"
 `include "Components/adder/Alu32b_extended_enumAluOp.vh"
-
-`define riscv32_1stage_pc_sel_pc_4 0
-`define riscv32_1stage_pc_sel_jalr 1
-`define riscv32_1stage_pc_sel_branch 2
-`define riscv32_1stage_pc_sel_jump 3
-`define riscv32_1stage_pc_sel_exception 4
-
-`define riscv32_1stage_op2Sel_pc 0
-`define riscv32_1stage_op2Sel_iTypeSignExtend 1
-`define riscv32_1stage_op2Sel_sTypeSignExtend 2
-`define riscv32_1stage_op2Sel_rs2 3
-`define riscv32_1stage_op2Sel_zero 4
-`define riscv32_1stage_op2Sel_shamtSignExtend 5
-
-`define riscv32_1stage_op1Sel_rs1 0
-`define riscv32_1stage_op1Sel_uTypeImmediate 1
-`define riscv32_1stage_op1Sel_four 2
-
-`define riscv32_1stage_wb_sel_oo_processor 0
-`define riscv32_1stage_wb_sel_pc_4 1
-`define riscv32_1stage_wb_sel_aluOut 2
-`define riscv32_1stage_wb_sel_memoryOut 3
+`include "Components/cpu/riscv/1stage/EnumSelections.vh"
 
 module RiscV1StageControl (
     instruction,
