1055e22cc69152891cad4899c1673f3a41651d83
Banyc <36535895+Banyc@users.noreply.github.com>
2020-12-17 17:52:25 +0800

    [cache] Done write cache storage

--- Components/memory/cache/directMapping/writeBack/Cache_512bytes_4bytes.v
+++ Components/memory/cache/directMapping/writeBack/Cache_512bytes_4bytes.v
@@ -37,7 +37,45 @@ module Cache_512bytes_4bytes (
     input  [31:0] mem_res_data;
     input         mem_res_valid;
     
-    
+    // data storage
+    // fields:
+    //   - data
+    //     - size: 32 bits
+    //   - tag
+    //     - size: 32 - log2(4 Bytes) - log2(512 Bytes / 4 Bytes) = 23 bits
+    //   - valid bit
+    //     - size: 1 bit
+    //   - dirty bit
+    //     - size: 1 bit
+    //   - row size: 57 bits
+    //   - column length: 128
+    reg [56:0] dataStorage [127:0];
+
+    wire addressTagField = cache_req_addr[31:9];
+    wire addressIndexField = cache_req_addr[8:2];
+
+    wire [56:0] dataStorageRowSelected = dataStorage[addressIndexField];
+    wire        validBit = dataStorageRowSelected[1];
+    wire        dirtyBit = dataStorageRowSelected[0];
+    wire [22:0] tagField = dataStorageRowSelected[25:2];
+    wire [31:0] dataField = dataStorageRowSelected[57:26];
+
+    wire isWriteDataStorage = cache_req_wen & 
+                   cache_req_valid &
+                   addressTagField == tagField &
+                   validBit;
+
+    always @(posedge clk or rst) begin
+        if (rst) begin
+            
+        end else begin
+            if (isWriteDataStorage) begin
+                dataStorage[addressIndexField][57:26] = cache_req_data;
+            end else begin
+                dataStorage[addressIndexField][57:26] = dataStorage[addressIndexField][57:26];
+            end
+        end
+    end
 
 endmodule
 
